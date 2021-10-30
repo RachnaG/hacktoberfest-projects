79aef2047b068c39592514b26fce31d334ce7ba5
RachnaG <53226805+RachnaG@users.noreply.github.com>
2021-10-30 14:19:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
+    domains:  ['avatars.githubusercontent.com', 'raw.githubusercontent.com']  ,
+    
+    
   },
+  
 };
